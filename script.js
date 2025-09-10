e795290148d7a02921e40744cd809ea31e4d9a01
Prajwal-Dabhade <prajwaldabhade13@gmail.com>
2025-09-10 19:47:15 +0530

    Add the file

---
+++ script.js
@@ -0,0 +1,7 @@
+ const typed = new Typed('.multiple-text', {
+      strings: ['Java Full Stack Developer', 'Front End Developer','Back End Developer','Full Stack Web Developer'],
+      typeSpeed: 60,
+      backSpeed: 60,
+      backDelay: 1000,
+      loop: true,
+    });
